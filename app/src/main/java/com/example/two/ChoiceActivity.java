e92b8d4b2a4beaca827399ac7f625107efbbe513
mugoori <120348468+mugoori@users.noreply.github.com>
2023-03-14 12:48:51 +0900

    03-14 화면 작업

--- app/src/main/java/com/example/two/ChoiceActivity.java
+++ app/src/main/java/com/example/two/ChoiceActivity.java
@@ -9,11 +9,7 @@ import android.widget.Button;
 
 public class ChoiceActivity extends AppCompatActivity {
 
-    Button btnCommunity;
-    Button btnHome;
-    Button btnFilter;
-    Button btnParty;
-    Button btnMy;
+
 
 
     @Override
@@ -21,61 +17,6 @@ public class ChoiceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
 
-        btnCommunity = findViewById(R.id.btnCommunity);
-        btnHome = findViewById(R.id.btnHome);
-        btnParty = findViewById(R.id.btnParty);
-        btnFilter = findViewById(R.id.btnFilter);
-        btnMy = findViewById(R.id.btnMy);
 
-        // 메인 액티비티 넘어가기
-        btnHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ChoiceActivity.this,MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        // 커뮤니티 액티비티 넘어가기
-        btnCommunity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ChoiceActivity.this,CommunityActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-
-        // 필터검색 액티비티 넘어가기
-        btnFilter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ChoiceActivity.this,FilterSearchActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        // 파티매칭 액티비티 넘어가기
-        btnParty.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ChoiceActivity.this,PartyActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        // 내정보 액티비티 넘어가기
-        btnMy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(ChoiceActivity.this,MyMenuActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
     }
 }
