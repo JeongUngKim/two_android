303f920a374764bc92cf390b42e4d77263ab069f
deyeon <deyeon209@gmail.com>
2023-03-16 14:19:04 +0900

    0316

--- app/src/main/java/com/example/two/config/RegisterApi.java
+++ app/src/main/java/com/example/two/config/RegisterApi.java
@@ -3,17 +3,27 @@ package com.example.two.config;
 import com.example.two.model.User;
 import com.example.two.model.UserRes;
 
+import org.json.JSONObject;
+
+import java.util.Map;
+
 import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Header;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
+import retrofit2.http.PartMap;
 
 public interface RegisterApi {
 
+    @Multipart
     @POST("register")
-    Call<UserRes> resgister(@Part MultipartBody.Part profileImg,@Part User user);
+    Call<UserRes> resgister(@PartMap JSONObject data,
+                            @Part MultipartBody.Part profileImg);
 
     @POST("/user/login")
     Call<UserRes> login(@Body User user);
