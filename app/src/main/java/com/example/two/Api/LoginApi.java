c756cf26f38bd536f74f788acbbfd053c569a1c7
JeongUngKim <120348508+JeongUngKim@users.noreply.github.com>
2023-03-19 19:27:02 +0900

    2023-03-19
    회원가입 처리

---
+++ app/src/main/java/com/example/two/Api/LoginApi.java
@@ -0,0 +1,30 @@
+package com.example.two.Api;
+
+import com.example.two.model.User;
+import com.example.two.model.UserRes;
+
+import java.util.HashMap;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+
+public interface LoginApi {
+    @POST("login")
+    Call<UserRes> Login(@Body User user);
+
+    @POST("Logout")
+    Call<HashMap<String,String>> Logout(@Header("Authorization") String token);
+
+    @POST("isId")
+    Call<HashMap<String,String>> IsId(@Body User user);
+
+    @POST("ispassword")
+    Call<HashMap<String,String>> Ispassword(@Body User user);
+
+    @PUT("changedpassword")
+    Call<HashMap<String,String>> Changepassword(@Body User user);
+}
