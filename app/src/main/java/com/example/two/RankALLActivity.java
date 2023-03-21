859934ebedc94570fc4c16e9ded1a550c1252934
mugoori <120348468+mugoori@users.noreply.github.com>
2023-03-21 10:58:17 +0900

    03/21 10:57 내정보 & 사이드바 작업

--- app/src/main/java/com/example/two/RankALLActivity.java
+++ app/src/main/java/com/example/two/RankALLActivity.java
@@ -2,13 +2,31 @@ package com.example.two;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class RankALLActivity extends AppCompatActivity {
 
+    ImageView imgBack;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rank_allactivity);
+
+        imgBack = findViewById(R.id.imgBack);
+
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RankALLActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
