bd12f3bc32c84e987dfc906b010c3b197f9e1d10
mugoori <120348468+mugoori@users.noreply.github.com>
2023-03-22 17:49:56 +0900

    03/22 17:49 내가 본 컨텐츠 작업중.

--- app/src/main/java/com/example/two/Api/ContentApi.java
+++ app/src/main/java/com/example/two/Api/ContentApi.java
@@ -1,10 +1,14 @@
 package com.example.two.Api;
 
+import com.example.two.model.ContentWatch;
+
+import retrofit2.Call;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface ContentApi {
 
-//    @POST("contentWatch/{contentId}")
-//    Call<> Mylike(@Header("Authorization") String token);
+    @POST("contentWatch/{contentId}")
+    Call<ContentWatch> contentWatch(@Header("Authorization") String token);
+
 }
