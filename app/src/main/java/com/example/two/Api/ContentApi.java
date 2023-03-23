16d8cb6659b514aa189e3cc794080a8907cdf178
mugoori <120348468+mugoori@users.noreply.github.com>
2023-03-23 12:42:18 +0900

    03/23 12:42 내가 본 컨텐츠 작업 완료

--- app/src/main/java/com/example/two/Api/ContentApi.java
+++ app/src/main/java/com/example/two/Api/ContentApi.java
@@ -1,14 +1,21 @@
 package com.example.two.Api;
 
 import com.example.two.model.ContentWatch;
+import com.example.two.model.ContentWatchList;
 
 import retrofit2.Call;
+import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface ContentApi {
 
     @POST("contentWatch/{contentId}")
-    Call<ContentWatch> contentWatch(@Header("Authorization") String token);
+    Call<ContentWatch> contentWatch(@Header("Authorization") String token, @Path("contentId") int contentId);
+
+    @GET("contentWatch")
+    Call<ContentWatchList> getContentWatch(@Header("Authorization") String token, @Query("page") int page);
 
 }
