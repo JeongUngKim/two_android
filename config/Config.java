8de127f1525606a3b70c3e2fcd34107d9a2b7bf8
gaeul Jeung <120348468+mugoori@users.noreply.github.com>
2023-04-05 10:17:00 +0900

    Add files via upload

---
+++ config/Config.java
@@ -0,0 +1,16 @@
+package com.example.two.config;
+
+import retrofit2.http.PUT;
+
+public class Config {
+    public static final String DOMAIN1 = "https://api.themoviedb.org/3/";
+    public static final String DOMAIN2 = "https://rti4bq5uqc.execute-api.ap-northeast-2.amazonaws.com/";
+
+    public static final String PREFERENCE_NAME ="TWO";
+
+    public static final String ACCESS_TOKEN ="access_token";
+    public static final String key = "ed885de901f9f30f8d5467da6c596eba";
+
+    // bootpay key
+    public static String access_key = "63f838513049c8001a365caa";
+}
